33ea85d4f687f19451e72c0f8c9ce373482caa20
fansan <fanin.s.pbl@gmail.com>
2021-12-08 18:54:37 +0530

    created folder udy added all files to it

---
+++ udy/cpr8.c
@@ -0,0 +1,10 @@
+//convert minutes to years and days
+#include<stdio.h>
+void main()
+{
+    float min,y,d;
+    printf("Enter the numberof minutes ");
+    scanf("%f",min);
+    d=min/(24*60);
+    
+}
