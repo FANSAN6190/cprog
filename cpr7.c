09bc6bd09245930ae911c1379fc152bf5439c7a0
fansan <fanin.s.pbl@gmail.com>
2021-12-08 18:41:13 +0530

    Files Modified /Progress 1

--- cpr7.c
+++ cpr7.c
@@ -2,11 +2,12 @@
 #include<stdbool.h>
 void main()
 {
-    int a=9,b=10;
+    int a=9,b=10,s;
     float c=2.5,d=4.5;
     printf("%f",b/c);
     printf("\n%f",a/d);
-    printf("\n%d",sizeof(int));
+    s=sizeof(int);
+    printf("\n%d",s);
 
 
 }
