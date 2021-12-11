32a41e16cd77b756100ae76632452c6c97ce47a4
FANSAN6190 <95154110+FANSAN6190@users.noreply.github.com>
2021-12-11 12:53:52 +0530

    Add files via upload

--- cprog8.c
+++ cprog8.c
@@ -1,1 +1,12 @@
-
+/*Temperature of a city in Fahrenheit degrees is input through the keyboard. Write a C
+program to convert this temperature into Centigrade degrees.
+[ C = (F – 32) / 1.8 ]*/
+#include<stdio.h>
+void main()
+{
+    float f,c;
+    printf("Enter the Temperature in Fahrenheit=");
+    scanf("%f",&f);
+    c=(f-32)/1.8;
+    printf("Temperature in Celsius= %d",c);
+}
